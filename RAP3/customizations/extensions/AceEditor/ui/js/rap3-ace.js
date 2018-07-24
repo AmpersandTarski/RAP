a0935675b5eda2b319e16d16e7683f957fd83d4d
Michiel Stornebrink <>
2018-07-24 14:51:06 +0200

    Fix for auto save after uploading a new script

--- RAP3/customizations/extensions/AceEditor/ui/js/rap3-ace.js
+++ RAP3/customizations/extensions/AceEditor/ui/js/rap3-ace.js
@@ -1,16 +1,19 @@
 var app = angular.module('AmpersandApp');
 app.requires[app.requires.length] = 'ui.ace';
-app.directive("fileread", [function() {
+app.directive("fileread", ['ResourceService', function(ResourceService) {
     return {
         scope: {
-            fileread: "="
+            resource: '=',
+            ifc: '=',
+            patchResource: '='
         },
         link: function(scope, element, attributes) {
             element.bind("change", function(changeEvent) {
                 var reader = new FileReader();
                 reader.onload = function(loadEvent) {
                     scope.$apply(function() {
-                        scope.fileread = loadEvent.target.result;
+                        scope.resource[scope.ifc] = loadEvent.target.result;
+                        ResourceService.saveItem(scope.resource, scope.ifc, scope.patchResource); // function(resource, ifc, patchResource)
                     });
                 }
                 reader.readAsText(changeEvent.target.files[0]);
