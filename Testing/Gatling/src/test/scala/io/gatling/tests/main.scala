444f9f5443b11997652fb8b96e9a9424788c1d4c
LiWeiYeh <liwei_yeh@hotmail.com>
2022-10-25 10:18:04 +0200

    added initial test without functions

--- Testing/Gatling/src/test/scala/io/gatling/tests/main.scala
+++ Testing/Gatling/src/test/scala/io/gatling/tests/main.scala
@@ -20,12 +20,45 @@ class Main extends Simulation {
     .acceptLanguageHeader("en-GB,en;q=0.9,en-US;q=0.8,nl;q=0.7")
     .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.47")
 
-  val login: Login = Login()
+  var scn =
+    scenario("Successful Login")
+      .exec(
+        http("Access Login Page")
+          .get("/api/v1/resource/SESSION/1/Login")
+          .check(jsonPath("$._id_").saveAs("sessId"))
+          .check(status.is(200))
+        )
+      .exec(
+        http("Enter correct credentials (username and password)")
+          .patch(s"/api/v1/resource/SESSION/1/")
+          .body(
+            StringBody(
+              """
+                |[
+                |  {
+                |    "op":"replace",
+                |    "path":"/Login/${sessId}/Login/${sessId}/Userid",
+                |    "value":"pinda"
+                |  },
+                |  {
+                |    "op":"replace",
+                |    "path":"/Login/${sessId}/Login/${sessId}/Password",
+                |    "value":"kaas"
+                |  },
+                |  {
+                |    "op":"replace",
+                |    "path":"/Login/${sessId}/Login/${sessId}/Login/property",
+                |    "value":true
+                |  }
+                |]
+                |""".stripMargin)).asJson
+        )
+      .exec(
+        http("Access MyScripts page when logged in")
+          .get("/api/v1/resource/SESSION/1/MyScripts")
+          .check(status.is(200))
+        )
 
   // Runs in parallel
-  setUp(
-    login.RunSuccessfulLogin()
-         .inject(atOnceUsers(1)).protocols(httpProtocol),
-    login.RunUnsuccessfulLogin()
-         .inject(atOnceUsers(1)).protocols(httpProtocol))
+  setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
 }
