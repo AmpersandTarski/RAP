44a889759fb37bcf1de2fbd1a9e5183255c6ce55
LiWeiYeh <liwei_yeh@hotmail.com>
2022-10-28 13:14:45 +0200

    added initial gatling tests with workflow (#175)
    * added initial gatling tests for login
    
    * added gatling cicd pipeline

---
+++ Testing/Gatling/src/test/scala/io/gatling/tests/requests/RAPRequests.scala
@@ -0,0 +1,28 @@
+package io.gatling.tests.requests
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+object RAPRequests {
+  val getHome = http("Access Home page")
+    .get("/")
+
+  val getLogin = http("Access Login Page")
+    .get("/api/v1/resource/SESSION/1/Login")
+    .check(jsonPath("$._id_").saveAs("sessId"))
+    .check(status.is(200))
+
+  val patchCorrectLogin = http("Enter correct credentials (username and password)")
+    .patch(s"/api/v1/resource/SESSION/1/")
+    .body(
+      ElFileBody("io/gatling/tests/requests/correct_login.json")).asJson
+
+  val patchIncorrectLogin = http("Enter incorrect credentials")
+    .patch(s"/api/v1/resource/SESSION/1/")
+    .body(
+      ElFileBody("io/gatling/tests/requests/incorrect_login.json")).asJson
+
+  val getMyScript = http("Access MyScripts page")
+    .get("/api/v1/resource/SESSION/1/MyScripts")
+}
---
+++ Testing/Gatling/src/test/scala/io/gatling/tests/scenarios/RAPScenarios.scala
@@ -0,0 +1,24 @@
+package io.gatling.tests.scenarios
+
+import io.gatling.tests.requests.RAPRequests
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+object RAPScenarios {
+  val runSuccessfulLogin =
+    scenario("Successful Login Scenario")
+      .exec(RAPRequests.getHome)
+      .exec(RAPRequests.getLogin)
+      .exec(RAPRequests.patchCorrectLogin)
+      .exec(RAPRequests.getMyScript
+                       .check(status.is(200)))
+
+  val runUnsuccessfulLogin =
+    scenario("Unsuccessful Login Scenario")
+      .exec(RAPRequests.getHome)
+      .exec(RAPRequests.getLogin)
+      .exec(RAPRequests.patchIncorrectLogin)
+      .exec(RAPRequests.getMyScript
+                       .check(status.is(401)))
+}
