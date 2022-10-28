f7abed12beb2f0528349098e07e30361cc0b5561
LiWeiYeh <liwei_yeh@hotmail.com>
2022-10-28 13:11:59 +0200

    resolved comments fr?

--- Testing/Gatling/src/test/scala/io/gatling/tests/simulations/UnsuccessfulLogin.scala
+++ Testing/Gatling/src/test/scala/io/gatling/tests/simulations/UnsuccessfulLogin.scala
@@ -2,9 +2,9 @@ package io.gatling.tests.simulations
 
 import io.gatling.tests.common.RAPDefaults
 import io.gatling.tests.scenarios.RAPScenarios
-import io.gatling.core.Predef.*
-import io.gatling.http.Predef.*
-import io.gatling.jdbc.Predef.*
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
 
 class UnsuccessfulLogin extends Simulation {
   setUp(
