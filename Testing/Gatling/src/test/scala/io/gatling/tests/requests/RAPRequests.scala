bd9a7157948bcd5215639b1f819156b3c89484d1
LiWeiYeh <liwei_yeh@hotmail.com>
2022-11-01 11:27:21 +0100

    changed request naming to the perspective of the user

--- Testing/Gatling/src/test/scala/io/gatling/tests/requests/RAPRequests.scala
+++ Testing/Gatling/src/test/scala/io/gatling/tests/requests/RAPRequests.scala
@@ -5,43 +5,43 @@ import io.gatling.http.Predef._
 import io.gatling.jdbc.Predef._
 
 object RAPRequests {
-  val getHome = http("Access Home page")
+  val getHome = http("User gets Home page")
     .get("/")
     .check(status.is(200))
 
-  val getLogin = http("Access Login Page")
+  val getLogin = http("User gets Login Page")
     .get("/api/v1/resource/SESSION/1/Login")
     .check(jsonPath("$._id_").saveAs("sessId"))
     .check(status.is(200))
 
-  val patchCorrectLogin = http("Enter correct credentials (username and password)")
+  val patchCorrectLogin = http("User enters correct credentials (username and password)")
     .patch(s"/api/v1/resource/SESSION/1/")
     .body(
       ElFileBody("io/gatling/tests/requests/correct_login.json")).asJson
 
-  val patchIncorrectLogin = http("Enter incorrect credentials")
+  val patchIncorrectLogin = http("User enters incorrect credentials")
     .patch(s"/api/v1/resource/SESSION/1/")
     .body(
       ElFileBody("io/gatling/tests/requests/incorrect_login.json")).asJson
 
-  val getMyScriptReturns200 = http("User can access MyScripts page")
+  val getMyScriptReturns200 = http("User can get MyScripts page")
     .get("/api/v1/resource/SESSION/1/MyScripts")
     .check(status.is(200))
 
-  val getMyScriptReturns401 = http("User cannot access MyScripts page")
+  val getMyScriptReturns401 = http("User cannot get MyScripts page")
     .get("/api/v1/resource/SESSION/1/MyScripts")
     .check(status.is(401))
 
-  val postNewScript = http("Post NewScript")
+  val postNewScript = http("User posts NewScript")
     .post("/api/v1/resource/Script")
     .check(jsonPath("$._id_").saveAs("scriptId"))
     .check(status.is(200))
 
-  val patchNewScript = http("Changing NewScript values and compile")
+  val patchNewScript = http("User changes NewScript values and compiles the script")
     .patch("/api/v1/resource/Script/Script_${scriptId}/Nieuw_32_script")
     .body(RawFileBody("io/gatling/tests/requests/new_script.json")).asJson
 
-  val getMyScriptsScriptId = http("Access the created NewScript from MyScripts page")
+  val getMyScriptsScriptId = http("User gets the newly created NewScript from MyScripts page")
     .get("/api/v1/resource/Script/Script_${scriptId}/Nieuw_32_script")
     .check(status.is(200))
 }
