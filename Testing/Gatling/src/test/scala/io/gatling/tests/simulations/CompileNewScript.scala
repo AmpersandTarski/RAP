4ca1a1bfd5662aedb0fbf5bf1416a0c20601c7d7
LiWeiYeh <liwei_yeh@hotmail.com>
2022-11-03 11:31:46 +0100

    fixed import errors

--- Testing/Gatling/src/test/scala/io/gatling/tests/simulations/CompileNewScript.scala
+++ Testing/Gatling/src/test/scala/io/gatling/tests/simulations/CompileNewScript.scala
@@ -1,8 +1,8 @@
 package io.gatling.tests.simulations
 
-import io.gatling.core.Predef.*
-import io.gatling.http.Predef.*
-import io.gatling.jdbc.Predef.*
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
 import io.gatling.tests.common.RAPDefaults
 import io.gatling.tests.scenarios.RAPScenarios
 
