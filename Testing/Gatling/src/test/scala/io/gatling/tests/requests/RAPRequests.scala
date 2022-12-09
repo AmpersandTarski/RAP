0043830581d303322d8ff92842ac4603d13d0aeb
Sharvin1 <35965804+Sharvin1@users.noreply.github.com>
2022-12-09 14:35:16 +0100

    Removed all status code except 200

--- Testing/Gatling/src/test/scala/io/gatling/tests/requests/RAPRequests.scala
+++ Testing/Gatling/src/test/scala/io/gatling/tests/requests/RAPRequests.scala
@@ -147,11 +147,5 @@ object RAPRequests {
 
   val checkDemoApplicationEnroll = http("Go to the test enroll prototype")
     .get(s"${RAPDefaults.ENROLL_URL}")
-    .check(status.not(500))
-    .check(status.not(400))
-    .check(status.not(401))
-    .check(status.not(403))
-    .check(status.not(404))
-    
     .check(status.is(200))
 }
