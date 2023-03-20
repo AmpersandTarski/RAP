f33e50d70767ea719f35e6492d4ee73f30debc91
WORK\fro33123 <freek.rodenburg@ordina.nl>
2023-03-20 16:30:48 +0100

    General tools to work with Kubernetes Cluster and Azure AKS

--- deployment/aks-general-tools.ps1
+++ deployment/aks-general-tools.ps1
@@ -3,6 +3,14 @@
 
 # CONNECT TO KUBERNETES CLUSTER
 
+# login to the correct directory
+az logout
+az login --tenant $Tenant
+# set correct subscription
+az account set -s $Sub
+# check your account/subscription
+echo (az account show --query=name)  # should return Ordina MTech
+
 # Get the credentials of the AKS Kubernetes Cluster and check the connectivity
 az aks get-credentials --resource-group $RG --name $AKSCluster --overwrite-existing
 
@@ -23,12 +31,62 @@ $NODENAME = (kubectl get nodes -o jsonpath='{.items[0].metadata.name}') # save n
 # The following command starts a privileged container on your node and connects to it.
 kubectl debug node/$NODENAME -it --image=mcr.microsoft.com/aks/fundamental/base-ubuntu:v0.0.11
 
-# AZURE: START/STOP/DELETE RESOURCE (GROUP)
+# KUBECTL: DELETE DEPLOYMENT
+
+# delete RAP
+kubectl delete namespace $NAMESPACE
+# delete cert-manager
+kubectl delete namespace cert-manager
+kubectl delete customresourcedefinition `
+    certificaterequests.cert-manager.io `
+    certificates.cert-manager.io `
+    challenges.acme.cert-manager.io `
+    clusterissuers.cert-manager.io `
+    issuers.cert-manager.io `
+    orders.acme.cert-manager.io
+kubectl delete clusterrole `
+    cert-manager-cainjector `
+    cert-manager-controller-approve:cert-manager-io `
+    cert-manager-controller-certificates `
+    cert-manager-controller-certificatesigningrequests `
+    cert-manager-controller-challenges `
+    cert-manager-controller-clusterissuers `
+    cert-manager-controller-ingress-shim `
+    cert-manager-controller-issuers `
+    cert-manager-controller-orders `
+    cert-manager-edit `
+    cert-manager-view `
+    kubectl delete clusterrolebinding `
+    cert-manager-webhook:subjectaccessreviews `
+    cert-manager-cainjector `
+    cert-manager-controller-approve:cert-manager-io `
+    cert-manager-controller-certificates `
+    cert-manager-controller-certificatesigningrequests `
+    cert-manager-controller-challenges `
+    cert-manager-controller-clusterissuers `
+    cert-manager-controller-ingress-shim `
+    cert-manager-controller-issuers `
+    cert-manager-controller-orders `
+    cert-manager-webhook:subjectaccessreviews
+kubectl delete rolebinding -n kube-system `
+    cert-manager-cainjector:leaderelection `
+    cert-manager:leaderelection
+kubectl delete MutatingWebhookConfiguration cert-manager-webhook
+kubectl delete ValidatingWebhookConfiguration cert-manager-webhook
+
+# delete ingress
+kubectl delete namespace ingress-nginx
+kubectl delete clusterrolebinding ingress-nginx
+kubectl delete clusterrole ingress-nginx
+kubectl delete ingressclass nginx
+kubectl delete validatingwebhookconfiguration ingress-nginx-admission
+
+# AZURE: START/STOP CLUSTER
 
 # start azure kubernetes cluster
 az aks start `
---resource-group $RG `
---name $AKSCluster
+    --resource-group $RG `
+    --name $AKSCluster
 
 # stop azure kubernetes cluster
 az aks stop `
@@ -58,12 +116,4 @@ az aks nodepool stop `
     --nodepool-name $NodePoolName `
     --resource-group $RG
 
-# delete node pool
-az aks nodepool delete `
-    --cluster-name $AKSCluster `
-    --nodepool-name $NodePoolName `
-    --resource-group $RG
-
-
-# delete resource group (and als resources)
-az group delete --resource-group $RG -y
+kubectl api-resources
