fabd54d50cf5f50f0cc3eab8e6eec10a65cdebff
Fran Slot <Fran.Slot@ordina.nl>
2023-12-12 14:56:05 +0100

    Added zip support to the student prototype image

--- RAP4USER/run-student-prototype.sh
+++ RAP4USER/run-student-prototype.sh
@@ -3,22 +3,31 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+main="$2"
+
+entry=$(echo -n $main | base64 -d)
+
 # Put content of stdin to file script.adl
 if [[ -n $RAP_DEPLOYMENT && $RAP_DEPLOYMENT == "Kubernetes" ]]; then
     # Kubernetes
-    echo "$1" | base64 -d > /script.adl
+    # echo "$1" | base64 -d > /script.adl
+    echo "$1" | base64 -d > /out.zip
+    unzip /out.zip -d /out
 else
     # Docker-compose
     base64 -d /dev/stdin > /script.adl
 fi
 
 # Print script for debugging purposes
-cat /script.adl
+# cat /script.adl
 
 # First generate Ampersand proto from student script
-ampersand proto /script.adl \
+ampersand proto $entry \
     --proto-dir=/var/www \
     --verbose
+# ampersand proto /script.adl \
+#     --proto-dir=/var/www \
+#     --verbose
 
 chown -R www-data:www-data /var/www/data /var/www/generics
 
